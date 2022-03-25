a1bcfbfff1b817512dd0eac3df88e5615856e7bf
James Francis <jfrancis@companieshouse.gov.uk>
2022-03-25 08:58:39 +0000

    See /#form-versions for details

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,8 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+router.post( '/register/beneficial-owner-statements', function ( req, res ) {
+		res.redirect( '/register/beneficial-owner-type' )
+} )
