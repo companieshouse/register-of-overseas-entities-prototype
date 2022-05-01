cce98c29ff988023c471002280dac1ae5448511d
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2022-05-01 17:40:51 +0100

    Error validation commit

--- app/routes.js
+++ app/routes.js
@@ -6,34 +6,10 @@ const router = express.Router()
 require('./routes/sign-in.js')(router)
 require('./routes/presenter.js')(router)
 require('./routes/due-diligence.js')(router)
-
-
-
-router.post( '/register/beneficial-owner-statements', function ( req, res ) {
-		res.redirect( '/register/beneficial-owner-type-alt' )
-} );
-
-
-router.post( '/register/beneficial-owner-type', function ( req, res ) {
-		res.redirect( '/register/beneficial-owner-individual' )
-} );
-
-
-
-router.get( '/register/beneficial-owner-individual', function ( req, res ) {
-	var boType = req.query.boType;
-	if ( boType == "beneficialTypesOther" ) {
-		res.redirect( "/register/beneficial-owner-other" );
-	} else if ( boType == "beneficialTypesGov" ) {
-		res.redirect( "/register/beneficial-owner-gov" );
-	} else if ( boType == "moTypesIndividual" ) {
-		res.redirect( "/register/managing-officer" );
-	} else if ( boType == "moTypesCorporate" ) {
-		res.redirect( "/register/managing-officer-corporate" );
-	} else {
-		res.render( 'register/beneficial-owner-individual' );
-	}
-} );
+require('./routes/entity.js')(router)
+require('./routes/beneficial-owner-statements.js')(router)
+require('./routes/beneficial-owner-type.js')(router)
+require('./routes/beneficial-owner-type-alt.js')(router)
 
 // Add your routes here - above the module.exports line
 
