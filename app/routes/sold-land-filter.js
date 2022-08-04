1e89a2348777b01f7e65a4dc86f36a5406d59ca9
James Francis <jfrancis@companieshouse.gov.uk>
2022-08-04 09:40:22 +0100

    Updates to Disposals

--- app/routes/sold-land-filter.js
+++ app/routes/sold-land-filter.js
@@ -3,7 +3,7 @@ module.exports = function (router) {
 
   router.get( '/register/secure-register-filter', function ( req, res ) {
 		var soldLand = req.query.soldLand;
-		if ( soldLand == "yes") {
+		if ( soldLand == "all") {
 			res.redirect( "/register/cannot-use" );
 		} else {
 			res.render( 'register/secure-register-filter' );
