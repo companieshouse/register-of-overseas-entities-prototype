36f39ceefe5f634c2de2d5350d9bb725329192c5
mark-bateman-ch <109281452+mark-bateman-ch@users.noreply.github.com>
2022-09-07 09:52:36 +0100

    CH Notification banner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,16 @@ $(document).ready(function () {
     return false;
   });
 })
+
+// Show and hide CH Notification Banner
+function chShowHideNotificationBanner() {
+  var x = document.getElementById("ch-notification-content"); // Hardcoded div ID is used as reference
+  var y = document.getElementById("ch-show-hide-button"); // Hardcoded button (faux link) ID is used as reference
+  if (x.style.display === "none") { // If the div is "none" (hidden) ...
+    x.style.display = "block"; // ... then show the div ...
+    y.innerHTML = "Hide message"; // ... and change button (link) label to 'Hide message'
+  } else {
+    x.style.display = "none"; // ... else hide the div ...
+    y.innerHTML = "Show message"; // ... and change button (link) label to 'Show message'
+  }
+}
