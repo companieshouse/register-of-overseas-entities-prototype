8970311742265c280b9687db61ef449073038ee5
mtuffs <mtuffs@companieshouse.gov.uk>
2022-10-27 13:24:05 +0100

    Routing updated

--- app/routes/add-trust.js
+++ app/routes/add-trust.js
@@ -1,15 +1,15 @@
 module.exports = function (router) {
 
 
-  router.get( '/register/trusts/add-trust', function ( req, res ) {
-		var dispChanged = req.query.dispChanged;
-		if ( dispChanged == "yes" ) {
-			res.redirect( "/register/trusts/trust-details" );
-		} else {
-			res.render( 'register/trusts/add-trust' );
-		}
-	} );
-
-}
-
+	router.get( '/register/check-answers', function ( req, res ) {
+		  var addTrust = req.query.addTrust;
+		  if ( addTrust == "yes" ) {
+			  res.redirect( "/register/trusts/trust-details" );
+		  } else {
+			  res.render( 'register/check-answers' );
+		  }
+	  } );
+  
+  }
+  
 
